91ec47d064574aa66ad76a4c7d1671dc0150b0f5
Đinh Viết Quân <104368461+Quann111@users.noreply.github.com>
2024-04-21 10:48:18 +0700

    ok

---
+++ print/urls.py
@@ -0,0 +1,17 @@
+
+
+
+from django.urls import path
+from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('import/', views.import_tickets, name='import_tickets'),
+    path('select/', views.import_selected_tickets, name='select_tickets'),
+    path('tickets/', views.ticket_list, name='ticket_list'),
+]
+
